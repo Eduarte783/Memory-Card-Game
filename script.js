a40c7c00e07db6fd31cc7f13f0704dfe062acfb4
Eduarte783 <eric.duarte83@gmail.com>
2022-05-11 15:40:06 -0700

    Update 5-11

--- script.js
+++ script.js
@@ -1,42 +1,95 @@
-const selectors = {
-    boardContainer: document.querySelector('.board-container'),
-    board: document.querySelector('.board'),
-    moves: document.querySelector('.moves'),
-    //timer: document.querySelector('.timer'),
-    start: document.querySelector('button'),
-    win: document.querySelector('.win')
-}
-const state = {
-    gameStart: false,
-    cardsFlipped: 0,
-    flipsTotal: 0,
-    timeTotal: 0,
-    loop: null
+const cards = document.querySelectorAll('.match-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  // second click
+  secondCard = this;
+  lockBoard = true;
+
+  checkForMatch();
 }
 
-const shuffle = array => {
-    const clonedArray = [...array]
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-    for (let index = clonedArray.length - 1; index > 0; index--) {
-        const randomIndex = Math.floor(Math.random() * (index +1))
-        const original = clonedArray[index]
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 20);
+    card.style.order = randomPos;
+  });
+
+
+  // Timer/Coundown
+
+ /* function countdown () {
         
-        clonedArray[index] = clonedArray[randomIndex]
-        clonedArray[randomIndex] = original
-    }
+    countdownStarted = true;
 
-    return clonedArray
-}
+    var timeStart = +new Date;
+    var timer = setInterval( function() {
+        
+        var timeNow = +new Date;
+        var difference = ( timeNow - timeStart ) / 1000; //calculates time difference if game isn't in focus
+        
+        if (time > 0 && !win) {// if there is still time left and game isn't won, deduct time
+            
+            time = 30;
+            time = Math.floor( time - difference );
+            $('.timer').text( time );
+            
+        } else {//stop timer when time is run out
+            
+            outOfTime = true;
+            alert("you have run out of time :(");
+            
+            clearInterval(timer);
+        } 
+        
+    }, 250 );
+    
+};*/
 
-const randomPick = (array, items) => {
-    const clonedArray = [...array]
-    const picksRandom = []
+});
 
-    for (let index = 0; index < items; index++) {
-        const randomIndex = Math.floor(Math.random() * clonedArray.length)
-
-        random.push(clonedArray[randomIndex])
-        clonedArray.splice(randomIndex, 1)
-    }
-    return picksRandom
-}
+cards.forEach(card => card.addEventListener('click', flipCard));
